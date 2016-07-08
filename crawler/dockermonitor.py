f4e5b98ac741fc6d2b880b343f720182ec5d009f
Shripad Nadgowda <nagowda@Shripads-MBP-2.home>
2016-07-08 09:56:42 -0400

    indentation fixed

--- crawler/dockermonitor.py
+++ crawler/dockermonitor.py
@@ -4,24 +4,24 @@ import docker
 
 class DockerMonitor(object):
     def __init__(self, eventQ):
-        self.eventQ = eventQ
-        self.client = docker.Client(base_url='unix://var/run/docker.sock')
+         self.eventQ = eventQ
+         self.client = docker.Client(base_url='unix://var/run/docker.sock')
 
     def __container_start_handler(self, event):
-        containerid = event['id']
-        imageid = event['from']
-        epochtime = event['time']
-        cEvent = ContainerEvent(containerid, imageid, event['Action'], epochtime)
-        self.eventQ.put(cEvent)
-
-     def __container_delete_handler(self, event):
          containerid = event['id']
          imageid = event['from']
          epochtime = event['time']
          cEvent = ContainerEvent(containerid, imageid, event['Action'], epochtime)
          self.eventQ.put(cEvent)
 
-     def startMonitor(self):
+    def __container_delete_handler(self, event):
+         containerid = event['id']
+         imageid = event['from']
+         epochtime = event['time']
+         cEvent = ContainerEvent(containerid, imageid, event['Action'], epochtime)
+         self.eventQ.put(cEvent)
+
+    def startMonitor(self):
          events = self.client.events(decode=True)
          for event in events:
              if event['Action'] == 'start':
