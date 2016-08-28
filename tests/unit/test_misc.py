050f01e3f0b47023788c952b197e7799523d256c
Sahil Suneja <sahilsuneja@gmail.com>
2016-08-28 15:55:41 -0400

    flake/pep tests/unit/*
    Signed-off-by: Sahil Suneja <sahilsuneja@gmail.com>

--- tests/unit/test_misc.py
+++ tests/unit/test_misc.py
@@ -7,32 +7,42 @@ import crawler.misc
 
 
 class MockedSocket1():
+
     def __init__(self, a, b):
         print a, b
         pass
+
     def connect(self, dest):
         pass
+
     def getsockname(self):
         return ['1.2.3.4']
 
+
 class MockedSocket2():
+
     def __init__(self, a, b):
         print a, b
         pass
+
     def connect(self, dest):
         pass
+
     def getsockname(self):
         raise socket.error()
+
     def gethostname(self):
         return '1.2.3.4'
 
 
 class MiscTests(unittest.TestCase):
+
     def test_find_mount_point(self, tmpdir='/'):
         assert crawler.misc.find_mount_point(str(tmpdir)) == '/'
 
     def test_subprocess_run(self):
-        assert crawler.misc.subprocess_run('echo abc', shell=True).strip() == 'abc'
+        assert crawler.misc.subprocess_run(
+            'echo abc', shell=True).strip() == 'abc'
         assert crawler.misc.subprocess_run('exit 0', shell=True).strip() == ''
         with self.assertRaises(RuntimeError):
             crawler.misc.subprocess_run('exit 1', shell=True)
@@ -44,15 +54,15 @@ class MiscTests(unittest.TestCase):
     def test_get_process_env(self, mock_open):
         mock_open.return_value = open('tests/unit/mock_environ_file')
         env = crawler.misc.get_process_env(pid=os.getpid())
-        assert env.has_key('HOME')
+        assert 'HOME' in env
         with self.assertRaises(TypeError):
             crawler.misc.get_process_env('asdf')
 
     def test_process_is_crawler(self):
-        assert crawler.misc.process_is_crawler(os.getpid()) == True
-        assert crawler.misc.process_is_crawler(1) == False
+        assert crawler.misc.process_is_crawler(os.getpid())
+        assert crawler.misc.process_is_crawler(1) is False
         # make sure 1123... does not actually exist
-        assert crawler.misc.process_is_crawler(1123234325123235) == False
+        assert crawler.misc.process_is_crawler(1123234325123235) is False
         with self.assertRaises(TypeError):
             crawler.misc.process_is_crawler('asdf')
 
@@ -60,10 +70,10 @@ class MiscTests(unittest.TestCase):
         assert '127.0.0.1' in crawler.misc.get_host_ip4_addresses()
 
     def test_is_process_running(self):
-        assert crawler.misc.is_process_running(os.getpid()) == True
-        assert crawler.misc.is_process_running(1) == True
+        assert crawler.misc.is_process_running(os.getpid())
+        assert crawler.misc.is_process_running(1)
         # make sure 1123... does not actually exist
-        assert crawler.misc.is_process_running(1123234325) == False
+        assert crawler.misc.is_process_running(1123234325) is False
         with self.assertRaises(TypeError):
             crawler.misc.is_process_running('asdf')
 
@@ -73,7 +83,7 @@ class MiscTests(unittest.TestCase):
 
     @mock.patch('crawler.misc.socket.socket', side_effect=MockedSocket2)
     @mock.patch('crawler.misc.socket.gethostname',
-                side_effect=lambda : '4.3.2.1')
+                side_effect=lambda: '4.3.2.1')
     def test_get_host_ipaddr2(self, *args):
         assert crawler.misc.get_host_ipaddr() == '4.3.2.1'
 
@@ -91,9 +101,40 @@ class MiscTests(unittest.TestCase):
 
     @mock.patch('crawler.misc.subprocess_run')
     def test_btrfs_list_subvolumes_with_list(self, mock_run):
-        mock_run.return_value = ('ID 257 gen 7 top level 5 path btrfs/subvolumes/a60a763cbaaedd3ac2b77bff939019fda876d8a187cb7e85789bb36377accbce\n'
-                                 'ID 258 gen 8 top level 5 path btrfs/subvolumes/9212798f6483145839c72f06a6bc2b0e456ca2b9ec14ea70e2948f098ce51077\n'
-                                 'ID 278 gen 1908 top level 5 path btrfs/subvolumes/7cd6c219c63e0282ddbd8437c9b2a0220aff40bbfd6734503bcd58e5afa28426\n')
+        mock_run.return_value = (
+            'ID 257 gen 7 top level 5 path btrfs/subvolumes/a60a763cbaaedd3ac2b77bff939019fda876d8a187cb7e85789bb36377accbce\n'
+            'ID 258 gen 8 top level 5 path btrfs/subvolumes/9212798f6483145839c72f06a6bc2b0e456ca2b9ec14ea70e2948f098ce51077\n'
+            'ID 278 gen 1908 top level 5 path btrfs/subvolumes/7cd6c219c63e0282ddbd8437c9b2a0220aff40bbfd6734503bcd58e5afa28426\n')
 
-        assert list(crawler.misc.btrfs_list_subvolumes('asd')) == [['ID', '257', 'gen', '7', 'top', 'level', '5', 'path', 'btrfs/subvolumes/a60a763cbaaedd3ac2b77bff939019fda876d8a187cb7e85789bb36377accbce'], ['ID', '258', 'gen', '8', 'top', 'level', '5', 'path', 'btrfs/subvolumes/9212798f6483145839c72f06a6bc2b0e456ca2b9ec14ea70e2948f098ce51077'], ['ID', '278', 'gen', '1908', 'top', 'level', '5', 'path', 'btrfs/subvolumes/7cd6c219c63e0282ddbd8437c9b2a0220aff40bbfd6734503bcd58e5afa28426']]
-
+        assert list(
+            crawler.misc.btrfs_list_subvolumes('asd')) == [
+            [
+                'ID',
+                '257',
+                'gen',
+                '7',
+                'top',
+                'level',
+                '5',
+                'path',
+                'btrfs/subvolumes/a60a763cbaaedd3ac2b77bff939019fda876d8a187cb7e85789bb36377accbce'],
+            [
+                'ID',
+                '258',
+                'gen',
+                '8',
+                'top',
+                'level',
+                '5',
+                'path',
+                'btrfs/subvolumes/9212798f6483145839c72f06a6bc2b0e456ca2b9ec14ea70e2948f098ce51077'],
+            [
+                'ID',
+                '278',
+                'gen',
+                '1908',
+                'top',
+                'level',
+                '5',
+                'path',
+                'btrfs/subvolumes/7cd6c219c63e0282ddbd8437c9b2a0220aff40bbfd6734503bcd58e5afa28426']]
--- tests/unit/test_package_utils.py
+++ tests/unit/test_package_utils.py
@@ -1,9 +1,9 @@
 import mock
 import unittest
-import os
 
 import crawler.package_utils
 from crawler.features import PackageFeature
+
 
 def mocked_subprocess_run(cmd, shell=False, ignore_failure=False):
     if 'dpkg-query' in cmd:
@@ -13,7 +13,9 @@ def mocked_subprocess_run(cmd, shell=False, ignore_failure=False):
         return ('123|pkg1|v1|x86|123\n'
                 '123|pkg1|v1|x86|123\n')
 
+
 class PackageUtilsTests(unittest.TestCase):
+
     def setUp(self):
         pass
 
@@ -25,19 +27,60 @@ class PackageUtilsTests(unittest.TestCase):
     def test_get_dpkg_packages(self, mock_subprocess_run):
         pkgs = list(crawler.package_utils.get_dpkg_packages())
         print pkgs
-        assert pkgs == [('pkg1', PackageFeature(installed=None, pkgname='pkg1', pkgsize='123', pkgversion='v1', pkgarchitecture='x86')), ('pkg2', PackageFeature(installed=None, pkgname='pkg2', pkgsize='123', pkgversion='v2', pkgarchitecture='x86'))]
+        assert pkgs == [
+            ('pkg1',
+             PackageFeature(
+                 installed=None,
+                 pkgname='pkg1',
+                 pkgsize='123',
+                 pkgversion='v1',
+                 pkgarchitecture='x86')),
+            ('pkg2',
+             PackageFeature(
+                 installed=None,
+                 pkgname='pkg2',
+                 pkgsize='123',
+                 pkgversion='v2',
+                 pkgarchitecture='x86'))]
 
     @mock.patch('crawler.package_utils.subprocess_run',
                 side_effect=mocked_subprocess_run)
     def test_get_rpm_packages(self, mock_subprocess_run):
         pkgs = list(crawler.package_utils.get_rpm_packages())
         print pkgs
-        assert pkgs == [('pkg1', PackageFeature(installed='123', pkgname='pkg1', pkgsize='123', pkgversion='v1', pkgarchitecture='x86')), ('pkg1', PackageFeature(installed='123', pkgname='pkg1', pkgsize='123', pkgversion='v1', pkgarchitecture='x86'))]
+        assert pkgs == [
+            ('pkg1',
+             PackageFeature(
+                 installed='123',
+                 pkgname='pkg1',
+                 pkgsize='123',
+                 pkgversion='v1',
+                 pkgarchitecture='x86')),
+            ('pkg1',
+             PackageFeature(
+                 installed='123',
+                 pkgname='pkg1',
+                 pkgsize='123',
+                 pkgversion='v1',
+                 pkgarchitecture='x86'))]
 
     @mock.patch('crawler.package_utils.subprocess_run',
                 side_effect=mocked_subprocess_run)
     def test_get_rpm_packages_with_db_reload(self, mock_subprocess_run):
         pkgs = list(crawler.package_utils.get_rpm_packages(reload_needed=True))
         print pkgs
-        assert pkgs == [('pkg1', PackageFeature(installed='123', pkgname='pkg1', pkgsize='123', pkgversion='v1', pkgarchitecture='x86')), ('pkg1', PackageFeature(installed='123', pkgname='pkg1', pkgsize='123', pkgversion='v1', pkgarchitecture='x86'))]
-
+        assert pkgs == [
+            ('pkg1',
+             PackageFeature(
+                 installed='123',
+                 pkgname='pkg1',
+                 pkgsize='123',
+                 pkgversion='v1',
+                 pkgarchitecture='x86')),
+            ('pkg1',
+             PackageFeature(
+                 installed='123',
+                 pkgname='pkg1',
+                 pkgsize='123',
+                 pkgversion='v1',
+                 pkgarchitecture='x86'))]
