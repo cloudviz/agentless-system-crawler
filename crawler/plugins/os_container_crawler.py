8747d57abe4bc61b03d2e049a66c15a9abe3e6fe
Ricardo Koller <kollerr@us.ibm.com>
2016-10-08 17:38:43 -0400

    Cleanup: os plugins, and MOUNTPOINT mode related code
    Signed-off-by: Ricardo Koller <kollerr@us.ibm.com>

--- crawler/plugins/os_container_crawler.py
+++ crawler/plugins/os_container_crawler.py
@@ -1,16 +1,8 @@
-import platform
-import misc
-import time
-import osinfo
 import dockerutils
 from icrawl_plugin import IContainerCrawler
-from features import OSFeature
 from namespace import run_as_another_namespace, ALL_NAMESPACES
+from plugins.os_crawler import crawl_os, crawl_os_mountpoint
 import logging
-
-# External dependencies that must be pip install'ed separately
-
-import psutil
 
 logger = logging.getLogger('crawlutils')
 
@@ -27,63 +19,9 @@ class OSContainerCrawler(IContainerCrawler):
         logger.debug('Crawling OS for container %s' % container_id)
 
         if avoid_setns:
-            return self._crawl_without_setns(container_id)
+            mp = dockerutils.get_docker_container_rootfs_path(container_id)
+            return crawl_os_mountpoint(mp)
         else:  # in all other cases, including wrong mode set
             return run_as_another_namespace(pid,
                                             ALL_NAMESPACES,
-                                            self._crawl_in_system)
-
-    def _crawl_in_system(self):
-        feature_key = platform.system().lower()
-        try:
-            os_kernel = platform.platform()
-        except:
-            os_kernel = 'unknown'
-
-        result = osinfo.get_osinfo(mount_point='/')
-        if result:
-            os_distro = result['os']
-            os_version = result['version']
-        else:
-            os_distro = 'unknown'
-            os_version = 'unknown'
-
-        ips = misc.get_host_ip4_addresses()
-
-        boot_time = psutil.boot_time()
-        uptime = int(time.time()) - boot_time
-        feature_attributes = OSFeature(
-            boot_time,
-            uptime,
-            ips,
-            os_distro,
-            os_version,
-            os_kernel,
-            platform.machine()
-        )
-
-        return [(feature_key, feature_attributes, 'os')]
-
-    def _crawl_without_setns(self, container_id):
-        mountpoint = dockerutils.get_docker_container_rootfs_path(container_id)
-
-        result = osinfo.get_osinfo(mount_point=mountpoint)
-        if result:
-            os_distro = result['os']
-            os_version = result['version']
-        else:
-            os_distro = 'unknown'
-            os_version = 'unknown'
-
-        feature_key = 'linux'
-        feature_attributes = OSFeature(  # boot time unknown for img
-                                         # live IP unknown for img
-            'unsupported',
-            'unsupported',
-            '0.0.0.0',
-            os_distro,
-            os_version,
-            'unknown',
-            'unknown'
-        )
-        return [(feature_key, feature_attributes, 'os')]
+                                            crawl_os)
