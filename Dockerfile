d29d5ec4eecae742a1b46a2bd8984c7565f61fcc
Stefan Berger <stefanb@linux.vnet.ibm.com>
2017-05-19 16:34:24 +0000

    Implement a ctprobe plugin using the more efficient conntrackprobe
    Implement a ctprobe plugin that is using the more efficient
    conntrackprobe for detecting container traffic.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,20 @@ COPY requirements.txt /crawler/requirements.txt
 RUN pip install -r requirements.txt
 
 COPY \
-  dependencies/python-socket-datacollector_0.1.1-1_all.deb \
+  dependencies/python-socket-datacollector_0.1.3-1_all.deb \
   dependencies/softflowd_0.9.9902-1_amd64.deb \
+  dependencies/python-conntrackprobe_0.1.0-1_all.deb \
   /tmp/
+
 RUN dpkg -i /tmp/python-socket-datacollector_*_all.deb && \
     apt-get -y update && \
     apt-get -y install libpcap0.8 && \
-    dpkg -i /tmp/softflowd_0.9.*amd64.deb && \
+    dpkg -i /tmp/softflowd_0.9.*.amd64.deb && \
+    pip install pyroute2 py-radix requests-unixsocket json-rpc && \
+    dpkg -i /tmp/python-conntrackprobe_*_all.deb && \
     rm -f /tmp/*.deb
+
+ENV PYTHONPATH=/usr/lib/python2.7/dist-packages:/usr/local/lib/python2.7/site-packages
 
 ADD crawler /crawler
 
---
+++ tests/functional/test_functional_ctprobe.py
@@ -0,0 +1,291 @@
+import json
+import logging
+import mock
+import os
+import shutil
+import sys
+import time
+import tempfile
+import unittest
+
+import docker
+import requests.exceptions
+from plugins.systems.ctprobe_container_crawler import CTProbeContainerCrawler
+from utils.process_utils import start_child
+
+
+# Tests the FprobeContainerCrawler class
+# Throws an AssertionError if any test fails
+
+CTPROBE_FRAME = \
+    '[{"data":"xAAAAAABAAYAAAAAAAAAAAIAAAA0AAGAFAABgAgAAQCsEQABCAACAKwRAA4cA' \
+    'AKABQABAAYAAAAGAAIAiEYAAAYAAwARWwAANAACgBQAAYAIAAEArBEADggAAgCsEQABHAAC' \
+    'gAUAAQAGAAAABgACABFbAAAGAAMAiEYAAAgADADhBU3ACAADAAAAAYgIAAcAAAAAeDAABIA' \
+    'sAAGABQABAAEAAAAFAAIABwAAAAUAAwAAAAAABgAEAAMAAAAGAAUAAAAAAA==","metadat' \
+    'a":{"ip-addresses":["172.17.0.14"]}},{"data":"jAAAAAIBAAAAAAAAAAAAAAIAA' \
+    'AA0AAGAFAABgAgAAQCsEQABCAACAKwRAA4cAAKABQABAAYAAAAGAAIAiDYAAAYAAwARWwAA' \
+    'NAACgBQAAYAIAAEArBEADggAAgCsEQABHAACgAUAAQAGAAAABgACABFbAAAGAAMAiDYAAAg' \
+    'ADAAM3QUACAADAAAAAY4=","metadata":{"ip-addresses":["172.17.0.14"]}}]'
+
+
+def simulate_socket_datacollector(params):
+    """ simulate writing by the socket-datacollector """
+    dir_idx = params.index('--dir')
+    assert dir_idx > 0
+    output_dir = params[dir_idx + 1]
+
+    filepattern_idx = params.index('--filepattern')
+    assert filepattern_idx > 0
+    filepattern = params[filepattern_idx + 1]
+
+    filename = os.path.join(output_dir, filepattern)
+    with open(filename, 'w') as f:
+        f.write(CTPROBE_FRAME)
+        print 'Write file %s' % filename
+    with open(filename + ".tmp", 'w') as f:
+        f.write(CTPROBE_FRAME)
+
+
+def mocked_start_child(params, pass_fds, null_fds, ign_sigs, setsid=False,
+                       **kwargs):
+    if params[0] == 'socket-datacollector':
+        # in case the socket-datacollector is started, we just write
+        # the frame without actually starting that program.
+        simulate_socket_datacollector(params)
+
+    # return appropriate values
+    return start_child(['sleep', '1'], pass_fds, null_fds, ign_sigs, setsid)
+
+
+def mocked_start_child_ctprobe_fail(params, pass_fds, null_fds, ign_sigs,
+                                    setsid=False, **kwargs):
+    if params[0] == 'ctprobe':
+        return start_child(['___no_such_file'], pass_fds, null_fds, ign_sigs,
+                           setsid, **kwargs)
+    return start_child(['sleep', '1'], pass_fds, null_fds, ign_sigs, setsid,
+                       **kwargs)
+
+
+def mocked_start_child_collector_fail(params, pass_fds, null_fds, ign_sigs,
+                                      setsid=False, **kwargs):
+    if params[0] == 'socket-datacollector':
+        return start_child(['___no_such_file'], pass_fds, null_fds, ign_sigs,
+                           setsid, **kwargs)
+    return start_child(['sleep', '1'], pass_fds, null_fds, ign_sigs,
+                       setsid, **kwargs)
+
+
+def mocked_session_get(self, path, data=''):
+    class Session(object):
+        def __init__(self, status_code, content):
+            self.status_code = status_code
+            self.content = json.dumps(content)
+
+    return Session(200, {'error': ''})
+
+
+def mocked_session_get_fail(self, path, data=''):
+    class Session(object):
+        def __init__(self, status_code, content):
+            self.status_code = status_code
+            self.content = json.dumps(content)
+
+    return Session(400, {'error': 'Bad request'})
+
+
+# Tests conducted with a single container running.
+class CtprobeFunctionalTests(unittest.TestCase):
+    image_name = 'alpine:latest'
+
+    def setUp(self):
+        self.docker = docker.Client(
+            base_url='unix://var/run/docker.sock', version='auto')
+        try:
+            if len(self.docker.containers()) != 0:
+                raise Exception(
+                    "Sorry, this test requires a machine with no docker"
+                    "containers running.")
+        except requests.exceptions.ConnectionError:
+            print ("Error connecting to docker daemon, are you in the docker"
+                   "group? You need to be in the docker group.")
+
+        self.docker.pull(repository='alpine', tag='latest')
+        self.container = self.docker.create_container(
+            image=self.image_name, command='ping -w 30 8.8.8.8')
+        self.tempd = tempfile.mkdtemp(prefix='crawlertest.')
+        self.docker.start(container=self.container['Id'])
+
+        self.output_dir = os.path.join(self.tempd, 'crawler-ctprobe')
+
+        self.params = {
+            'ctprobe_user': 'nobody',
+            'ctprobe_output_dir': self.output_dir,
+            'output_filepattern': 'testfile',
+        }
+
+        logging.basicConfig(stream=sys.stderr)
+        self.logger = logging.getLogger("crawlutils").setLevel(logging.INFO)
+
+    def tearDown(self):
+        self.docker.stop(container=self.container['Id'])
+        self.docker.remove_container(container=self.container['Id'])
+
+        shutil.rmtree(self.tempd)
+
+    @mock.patch('plugins.systems.ctprobe_container_crawler.start_child',
+                mocked_start_child)
+    @mock.patch('plugins.systems.ctprobe_container_crawler.'
+                'requests_unixsocket.Session.get', mocked_session_get)
+    def test_crawl_outcontainer_ctprobe(self):
+        logger = logging.getLogger("crawlutils")
+        logger.info('>>> Testcase: expecting collector output')
+
+        num = len(CTProbeContainerCrawler.ifaces_monitored)
+
+        ctc = CTProbeContainerCrawler()
+        assert ctc.get_feature() == 'ctprobe'
+
+        # the fake collector writes the single frame immediately
+        res = []
+        for data in ctc.crawl(self.container['Id'], avoid_setns=False,
+                              **self.params):
+            res.append(data)
+        assert len(res) == 1
+        assert len(CTProbeContainerCrawler.ifaces_monitored) == num + 1
+
+    @mock.patch('plugins.systems.ctprobe_container_crawler.start_child',
+                mocked_start_child)
+    @mock.patch('plugins.systems.ctprobe_container_crawler.'
+                'requests_unixsocket.Session.get', mocked_session_get_fail)
+    def test_start_netlink_collection_fault1(self):
+        logger = logging.getLogger("crawlutils")
+        logger.info('>>> Testcase: collector cannot be configured')
+
+        num = len(CTProbeContainerCrawler.ifaces_monitored)
+
+        ctc = CTProbeContainerCrawler()
+        assert ctc.get_feature() == 'ctprobe'
+
+        # with ctprobe failing to start, we won't get data
+        res = []
+        for data in ctc.crawl(self.container['Id'], avoid_setns=False,
+                              **self.params):
+            res.append(data)
+        assert len(res) == 1
+        assert len(CTProbeContainerCrawler.ifaces_monitored) == num
+
+    @mock.patch('plugins.systems.ctprobe_container_crawler.start_child',
+                mocked_start_child_collector_fail)
+    @mock.patch('plugins.systems.ctprobe_container_crawler.'
+                'requests_unixsocket.Session.get', mocked_session_get)
+    def test_start_netlink_collection_fault2(self):
+        logger = logging.getLogger("crawlutils")
+        logger.info('>>> Testcase: collector fails to start')
+
+        num = len(CTProbeContainerCrawler.ifaces_monitored)
+
+        ctc = CTProbeContainerCrawler()
+        assert ctc.get_feature() == 'ctprobe'
+
+        # with socket-datacollector failing to start, we won't get data
+        res = []
+        for data in ctc.crawl(self.container['Id'], avoid_setns=False,
+                              **self.params):
+            res.append(data)
+        assert len(res) == 0
+        assert len(CTProbeContainerCrawler.ifaces_monitored) == num
+
+    @mock.patch('plugins.systems.ctprobe_container_crawler.start_child',
+                mocked_start_child)
+    @mock.patch('plugins.systems.ctprobe_container_crawler.'
+                'requests_unixsocket.Session.get', mocked_session_get)
+    def test_remove_datafiles(self):
+        logger = logging.getLogger("crawlutils")
+        logger.info('>>> Testcase: datafiles of disappeared interface '
+                    'being removed')
+
+        ctc = CTProbeContainerCrawler()
+        assert ctc.get_feature() == 'ctprobe'
+
+        # we pretend that an interface test.eth0 existed
+        ifname = 'test.eth0'
+        CTProbeContainerCrawler.ifaces_monitored.append(ifname)
+
+        self.params['output_filepattern'] = 'ctprobe-{ifname}-{timestamp}'
+
+        # create a datafile for this fake interface
+        timestamp = int(time.time())
+        filepattern = 'ctprobe-{ifname}-{timestamp}'.format(ifname=ifname,
+                                                            timestamp=timestamp)
+        params = [
+            'socket-datacollector',
+            '--dir', self.output_dir,
+            '--filepattern', filepattern,
+        ]
+
+        # have the fake socket-datacollector write a file with the ifname in
+        # the filename
+        ctc.setup_outputdir(self.output_dir, os.getuid(), os.getgid())
+        simulate_socket_datacollector(params)
+        written_file = os.path.join(self.output_dir, filepattern)
+        assert os.path.isfile(written_file)
+
+        CTProbeContainerCrawler.next_cleanup = 0
+        # calling ctc.crawl() will trigger a cleanup of that file
+        # since our fake interface never existed
+        ctc.crawl(self.container['Id'], avoid_setns=False, **self.params)
+
+        # file should be gone now
+        assert not os.path.isfile(written_file)
+
+    @mock.patch('plugins.systems.ctprobe_container_crawler.start_child',
+                mocked_start_child)
+    @mock.patch('plugins.systems.ctprobe_container_crawler.'
+                'requests_unixsocket.Session.get', mocked_session_get)
+    def test_remove_stale_files(self):
+        logger = logging.getLogger("crawlutils")
+        logger.info('>>> Testcase: stale file being removed')
+
+        ctc = CTProbeContainerCrawler()
+        assert ctc.get_feature() == 'ctprobe'
+
+        # we pretend that an interface test.eth0 existed
+        ifname = 'test.eth0'
+        CTProbeContainerCrawler.ifaces_monitored.append(ifname)
+
+        self.params['output_filepattern'] = 'ctprobe-{ifname}-{timestamp}'
+
+        # have the fake socket-datacollector write a file with the ifname in
+        # the filename
+        ctc.setup_outputdir(self.output_dir, os.getuid(), os.getgid())
+
+        written_file = os.path.join(self.output_dir, 'test.output')
+        with open(written_file, 'a') as f:
+            f.write('hello')
+
+        assert os.path.isfile(written_file)
+
+        # mock the stale file timeout so that our file will get removed
+        # with in reasonable time
+        CTProbeContainerCrawler.STALE_FILE_TIMEOUT = 5
+
+        # calling ctc.crawl() will not trigger a cleanup of that file
+        # the first time
+        logger.info('1st crawl')
+        ctc.crawl(self.container['Id'], avoid_setns=False, **self.params)
+
+        # file should still be here
+        assert os.path.isfile(written_file)
+
+        # the next time we will crawl, the file will be removed
+        CTProbeContainerCrawler.next_cleanup = time.time()
+        time.sleep(CTProbeContainerCrawler.STALE_FILE_TIMEOUT + 1)
+
+        logger.info('2nd crawl')
+        ctc.crawl(self.container['Id'], avoid_setns=False, **self.params)
+
+        # file should be gone now
+        assert not os.path.isfile(written_file)
+
+    if __name__ == '__main__':
+        unittest.main()
