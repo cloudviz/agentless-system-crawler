c12e200c8e0084c5ceff09c57db73ec1699ee01c
Sastry Duri <sastry@us.ibm.com>
2017-11-01 15:37:34 -0400

    added a new test case
    Signed-off-by: Sastry Duri <sastry@us.ibm.com>

--- tests/unit/test_plugins.py
+++ tests/unit/test_plugins.py
@@ -654,6 +654,45 @@ class PluginTests(unittest.TestCase):
         finally:
             os.remove(path)
 
+
+    @mock.patch(
+        ("plugins.systems.jar_container_crawler."
+            "utils.dockerutils.exec_dockerinspect"),
+        side_effect=lambda long_id: {'State': {'Pid': 123}})
+    @mock.patch(
+        ("plugins.systems.jar_container_crawler.utils.dockerutils."
+            "get_docker_container_rootfs_path"),
+        side_effect=lambda long_id: '/1/2/3')
+    def test_jar_container_crawler_avoidsetns(self, *args):
+        tmpdir = tempfile.mkdtemp()
+        jar_file_name = 'myfile.jar'
+
+        # Ensure the file is read/write by the creator only
+        saved_umask = os.umask(0077)
+
+        path = os.path.join(tmpdir, jar_file_name)
+        try:
+            with ZipFile(path, "w") as myjar:
+                myjar.writestr(ZipInfo('first.class',(1980,1,1,1,1,1)), "first secrets!")
+                myjar.writestr(ZipInfo('second.class',(1980,1,1,1,1,1)), "second secrets!")
+                myjar.writestr(ZipInfo('second.txt',(1980,1,1,1,1,1)), "second secrets!")
+
+            fc = JarContainerCrawler()
+            jars = list(fc.crawl(root_dir=tmpdir))
+            #jars = list(jar_utils.crawl_jar_files(root_dir=tmpdir))
+            print jars
+            jar_feature = jars[0][1]
+            assert 'myfile.jar' == jar_feature.name
+            assert '48ac85a26ffa7ff5cefdd5c73a9fb888' == jar_feature.jarhash
+            assert ['ddc6eff37020aa858e26b1ba8a49ee0e',
+                    'cbe2a13eb99c1c8ac5f30d0a04f8c492'] == jar_feature.hashes
+            assert 'jar' == jars[0][2]
+
+        except IOError as e:
+            print 'IOError'
+        finally:
+            os.remove(path)
+
     @mock.patch(
         ("plugins.systems.file_container_crawler."
             "utils.dockerutils.exec_dockerinspect"),
