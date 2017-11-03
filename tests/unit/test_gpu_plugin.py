b2dd41acfad3f9a5132b9e74749bc6b5b6d60967
Shripad Nadgowda <nadgowda@us.ibm.com>
2017-11-03 09:53:50 -0400

    Dlaas metric metadata changes
    Signed-off-by: Shripad Nadgowda <nadgowda@us.ibm.com>

--- tests/unit/test_gpu_plugin.py
+++ tests/unit/test_gpu_plugin.py
@@ -5,6 +5,8 @@ sys.path.append('tests/unit/')
 sys.modules['pynvml'] = __import__('mock_pynvml')
 from plugins.systems.gpu_host_crawler import GPUHostCrawler
 
+def mocked_util():
+    return "127.0.0.1"
 
 class GPUPluginTests(unittest.TestCase):
 
@@ -17,16 +19,17 @@ class GPUPluginTests(unittest.TestCase):
     @mock.patch(
         'plugins.systems.gpu_host_crawler.GPUHostCrawler._load_nvidia_lib',
         side_effect=lambda: 1)
+    @mock.patch('utils.misc.get_host_ipaddr',side_effect=mocked_util)
     def test_os_gpu_host_crawler_plugin(self, *args):
         fc = GPUHostCrawler()
         for gpu_metrics in fc.crawl():
             print gpu_metrics
-            assert gpu_metrics == (
-                'gpu0.containerid:NA',
+            assert (
+                'gpu0.NA',
                 {
                     "memory": {"total": 12205, "used": 0, "free": 12205},
                     "temperature": 31,
                     "power": {"draw": 27, "limit": 149},
                     "utilization": {"gpu": 0, "memory": 0}
                 },
-                'gpu')
+                'gpu') in gpu_metrics
