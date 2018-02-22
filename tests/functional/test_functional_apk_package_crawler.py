9f6713865c183d2450594da8339dde21a69c9726
Sahil Suneja <sahilsuneja@gmail.com>
2018-02-22 11:17:46 -0500

    merge fix for new docker version
    Signed-off-by: Sahil Suneja <sahilsuneja@gmail.com>

--- tests/functional/test_functional_apk_package_crawler.py
+++ tests/functional/test_functional_apk_package_crawler.py
@@ -31,7 +31,7 @@ class ContainersCrawlerTests(unittest.TestCase):
         ch.setFormatter(formatter)
         root.addHandler(ch)
 
-        self.docker = docker.Client(base_url='unix://var/run/docker.sock',
+        self.docker = docker.APIClient(base_url='unix://var/run/docker.sock',
                                     version='auto')
         try:
             if len(self.docker.containers()) != 0:
